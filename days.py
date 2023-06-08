23e47414ea03c8df93d28dceecde9885a948bcae
Dario127 <vikashchauhan127@gmail.com>
2023-06-08 13:32:48 +0530

    added calculation file

---
+++ days.py
@@ -0,0 +1,11 @@
+calculation_to_units = 24
+name_of_units = "hours"
+
+def days_of_units(num_of_days):
+    print(f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_units}")
+    print("calm down")
+    
+days_of_units(45)
+days_of_units(55)
+days_of_units(65)
+days_of_units(75)
