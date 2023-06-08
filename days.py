23210c4a58c53ec56ce7faab3b3c1ffc772e8ecd
Dario127 <vikashchauhan127@gmail.com>
2023-06-08 13:54:33 +0530

    days changed to 90

--- days.py
+++ days.py
@@ -4,8 +4,6 @@ name_of_units = "hours"
 def days_of_units(num_of_days):
     print(f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_units}")
     print("calm down")
+    print("Dekh lenege apko bhi")
     
-days_of_units(45)
-days_of_units(55)
-days_of_units(65)
-days_of_units(75)
+days_of_units(90)
